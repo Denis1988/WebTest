21fea0df5fc6ed5959a97bf0fda51fdd0fc3278d
Denis1988 <denis1988@gmail.com>
2018-05-21 15:36:14 +0300

    Update app.js

--- Vue/MultyApp/app.js
+++ Vue/MultyApp/app.js
@@ -1,16 +1,53 @@
+var message = [];
+
 var chat = new Vue({
   el:'#chat',
   data: {
-    title:'hello'
+    message:[{
+      text:'Blah Blah',
+      time:"2:12:55 AM 21.5.18"
+    },
+    {
+      text:'Blah Blah1',
+      time:"2:13:55 AM 21.5.18"
+    },{
+      text:'Blah Blah2',
+      time:"2:14:55 AM 21.5.18"
+    },{
+      text:'Blah Blah3',
+      time:"2:15:55 AM 21.5.18"
+    },]
   }
 
 });
 
+var user = [];
+
 var users = new Vue({
   el:'#users',
   data: {
-    title:'hello'
-  }
+    user : [{
+      name: 'Denis',
+      ip:'localhost',
+      lastConnectedTime:'2018:21:05 8:37 AM',
+      lastMessage:'Hello World',
+      avatar:'SomeLinkToImage'
+    },
+    {
+      name: 'Denis1',
+      ip:'localhost1',
+      lastConnectedTime:'2018:21:05 8:37 AM',
+      lastMessage:'Hello World1',
+      avatar:'SomeLinkToImage1'
+    },
+    {
+      name: 'Denis2',
+      ip:'localhost2',
+      lastConnectedTime:'2018:21:05 8:37 AM',
+      lastMessage:'Hello World1',
+      avatar:'SomeLinkToImage1'
+    }]
+
   }
 
 });
